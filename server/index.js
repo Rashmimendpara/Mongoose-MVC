0be9a9bad0eabb07652a141d6d328ef179c53bfc
RashmiMendpara <rashmimendpara2020@gmail.com>
2023-08-13 23:49:00 +0200

    Changes in index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use('/api/books', booksRouter);
 
 //Deployment
 if(process.env.NODE_ENV === 'production'){
-    const buildPath = path.join(_dirname, '../client/dist');
+    const buildPath = path.join(_dirname, './client/dist');
     app.use(express.static(buildPath));
 
     app.get('*', (req,res) => res.sendFile(path.join(buildPath, 'index.html')));
